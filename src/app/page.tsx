9494c5c42bc335db3ab86dff6ea95043940c98d0
Todkhuu <todoo.zorigtbaatar@gmail.com>
2025-01-20 19:43:11 +0800

    popular

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import Star from "./Star";
+import { Popular } from "@/components/Popular";
 
 type MovieType = {
   adult: boolean;
@@ -48,37 +49,40 @@ export default function Home() {
   console.log({ movies });
   return (
     <div className="max-w-[1280px] m-auto flex flex-wrap">
-      <div className="w-[100%] flex justify-between my-[32px]">
-        <h2>Upcoming</h2> <p>See more </p>
-      </div>
-      <div className="flex flex-wrap gap-[32px]">
-        {movies?.slice(0, 10).map((movie, index) => {
-          return (
-            <div
-              key={index}
-              className="w-[230px] h-[440px] bg-[#F5F5F5] justify-around rounded-[8px] overflow-hidden"
-            >
-              <img
-                className="w-[100%] h-[75%]"
-                src={`https://image.tmdb.org/t/p/original/${movie?.poster_path}`}
-                alt=""
-              />
-              <div className="p-[8px] flex flex-col gap-[8px]">
-                <div className="flex items-center gap-[4px]">
-                  <Star />
-                  <p className="text-[14px] text-[#09090b] font-medium ">
-                    {movie?.vote_average}
-                    <span className="text-[#71717a] text-[12px]">/10</span>
-                  </p>
+      <div>
+        <div className="w-[100%] flex justify-between my-[32px]">
+          <h2>Upcoming</h2> <p>See more </p>
+        </div>
+        <div className="flex flex-wrap gap-[32px]">
+          {movies?.slice(0, 10).map((movie, index) => {
+            return (
+              <div
+                key={index}
+                className="w-[230px] h-[440px] bg-[#F5F5F5] justify-around rounded-[8px] overflow-hidden"
+              >
+                <img
+                  className="w-[100%] h-[75%]"
+                  src={`https://image.tmdb.org/t/p/original/${movie?.poster_path}`}
+                  alt=""
+                />
+                <div className="p-[8px] flex flex-col gap-[8px]">
+                  <div className="flex items-center gap-[4px]">
+                    <Star />
+                    <p className="text-[14px] text-[#09090b] font-medium ">
+                      {movie?.vote_average}
+                      <span className="text-[#71717a] text-[12px]">/10</span>
+                    </p>
+                  </div>
+                  <h2 className="text-[#09090b] text-[18px] font-normal ">
+                    {movie?.title}
+                  </h2>
                 </div>
-                <h2 className="text-[#09090b] text-[18px] font-normal ">
-                  {movie?.title}
-                </h2>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
+      <Popular />
     </div>
   );
 }
