270ed29d87916b01995999e6a314e8d7f97ac856
Todkhuu <todoo.zorigtbaatar@gmail.com>
2025-01-22 21:07:45 +0800

    hmmm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,10 @@ import { Upcoming } from "./_components/Upcoming";
 import { Popular } from "./_components/Popular";
 import { TopRated } from "./_components/TopRated";
 import { TopCard } from "./_components/TopCard";
-import { ModeToggle } from "@/components/theme-toggle";
 import { CarouselDemo } from "@/components/carousel";
 import { Header } from "./_components/Header";
 
-export default async function Home() {
+export default function Home() {
   return (
     <div>
       <Header />
---
+++ src/components/Skeleton.tsx
@@ -0,0 +1,9 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export function SkeletonSlider() {
+  return (
+    <div>
+      <Skeleton className="h-[600px] w-[90%] rounded-xl" />
+    </div>
+  );
+}
