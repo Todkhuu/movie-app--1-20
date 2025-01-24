f1466f685ec6e6eea64a1b292500384f086191cb
Todkhuu <todoo.zorigtbaatar@gmail.com>
2025-01-24 08:03:22 +0800

    key

--- src/app/movie-detail/[movieId]/components/Card.tsx
+++ src/app/movie-detail/[movieId]/components/Card.tsx
@@ -10,25 +10,28 @@ type Card = {
 export const Cards = ({ dataCard }: Card) => {
   return (
     <div className="flex gap-[32px]">
-      {dataCard.slice(0, 5).map((d: MovieType) => {
+      {dataCard.slice(0, 5).map((card: MovieType, index: number) => {
         return (
-          <Card className="w-[190px] justify-around overflow-hidden bg-secondary hover:opacity-60 linear rounded-xl">
+          <Card
+            key={index}
+            className="w-[190px] justify-around overflow-hidden bg-secondary hover:opacity-60 linear rounded-xl"
+          >
             <Image
-              src={`https://image.tmdb.org/t/p/original/${d?.poster_path}`}
+              src={`https://image.tmdb.org/t/p/original/${card?.poster_path}`}
               width={1000}
               height={1000}
-              alt={d.title || "Movie poster"}
+              alt={card.title || "Movie poster"}
             />
             <div className="p-[8px] flex flex-col gap-[8px]">
               <div className="flex items-center gap-[4px]">
                 <Star />
                 <p className="text-[14px] font-medium ">
-                  {d?.vote_average.toFixed(1)}
+                  {card?.vote_average.toFixed(1)}
                   <span className="text-[#71717a] text-[12px]">/10</span>
                 </p>
               </div>
               <CardTitle className=" text-[18px] font-normal ">
-                {d?.title}
+                {card?.title}
               </CardTitle>
             </div>
           </Card>
--- src/app/movie-detail/[movieId]/components/Genres.tsx
+++ src/app/movie-detail/[movieId]/components/Genres.tsx
@@ -7,10 +7,13 @@ type Genre = {
 export const Genres = ({ dataGenre }: Genre) => {
   return (
     <div className="mt-[32px] flex gap-[12px]">
-      {dataGenre.slice(0, 4).map((data: MovieType) => {
+      {dataGenre.slice(0, 4).map((genre: MovieType, index: number) => {
         return (
-          <button className="rounded-[15px] border-white-400 border-solid border-[1px] px-[10px] py-[2px]">
-            <p className="font-medium text-[12px]">{data?.name}</p>
+          <button
+            key={index}
+            className="rounded-[15px] border-white-400 border-solid border-[1px] px-[10px] py-[2px]"
+          >
+            <p className="font-medium text-[12px]">{genre?.name}</p>
           </button>
         );
       })}
